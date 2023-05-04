fc1e070a607a05af4ae45c5ce4ddb2665e2a44e2
Elian <eliannben2@gmail.com>
2023-05-04 19:26:20 -0300

    First commit

---
+++ src/test/java/com/testing2023/grupo1/Grupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing2023.grupo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
